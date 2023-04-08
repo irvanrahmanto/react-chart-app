cfd69529b14b342d9194412f0e7665f963185f46
Irvan Rahmanto <irvanrahmanto@Irvans-MacBook-Pro.local>
2023-04-08 17:16:46 +0700

    passing dynamic data using props

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-function ExpensesItem() {
-    const expenseDate = new Date(2023, 2, 28);
+function ExpensesItem(props) {
+    const expenseDate = new Date(2023, 3, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
 
     return (
         <div className='expense-item'>
-            <div>March 27th 2022</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>IDR 100.000</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
