0b3a4e47241d9f05e760aa016bf888cacd79d08c
Irvan Rahmanto <irvanrahmanto@Irvans-MacBook-Pro.local>
2023-04-08 17:58:33 +0700

    adding logic into component & splitting

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,14 +1,11 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpensesItem(props) {
-    const expenseDate = new Date(2023, 3, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
